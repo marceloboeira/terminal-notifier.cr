b6024137227b9e845a2b69fbb518b1bf1d5ab265
Marcelo Boeira <me@marceloboeira.com>
2016-02-10 02:25:03 -0200

    fix: Rename correctly to terminal-notifier

---
+++ src/terminal-notifier.cr
@@ -0,0 +1,22 @@
+module TerminalNotifier
+  VERSION = "0.0.3"
+  BIN_PATH = "terminal-notifier"
+
+  def self.notify(message : String)
+    TerminalNotifier.execute({ message: message })
+  end
+
+  def self.notify(message : String, title : String)
+    TerminalNotifier.execute({ message: message, title: title })
+  end
+
+  def self.execute(params : Hash)
+    options = ""
+
+    params.each do |key, value|
+      options += "-#{key} #{value} "
+    end
+
+    `#{BIN_PATH} #{options}`
+  end
+end
