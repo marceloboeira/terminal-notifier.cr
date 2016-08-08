113e7d24ccf6a8be9689e7b808f010a11f0d3dff
Barry Allard <barry.allard@gmail.com>
2016-08-08 13:33:33 -0700

    rewrite: more feedback, options

--- src/terminal-notifier.cr
+++ src/terminal-notifier.cr
@@ -1,22 +1,37 @@
 module TerminalNotifier
-  VERSION = "0.1.0"
+  VERSION = "0.2.0"
   BIN_PATH = "terminal-notifier"
 
-  def self.notify(message : String)
-    TerminalNotifier.execute({ message: message })
+  def self.notify(message       : String,
+                  title         : String = nil,
+                  subtitle      : String = nil,
+                  app_icon      : String = nil,
+                  content_image : String = nil)
+    params = { message: message }
+    params[:title]        = title         if title
+    params[:subtitle]     = subtitle      if subtitle
+    # Mavericks+
+    params[:appIcon]      = app_icon      if app_icon
+    params[:contentImage] = content_image if content_image
+
+    execute(params)
   end
 
-  def self.notify(message : String, title : String)
-    TerminalNotifier.execute({ message: message, title: title })
-  end
+  def self.execute(params : NamedTuple)
+    check_execute
 
-  def self.execute(params : Hash)
-    options = ""
-
-    params.each do |key, value|
-      options += "-#{key} #{value.inspect} "
-    end
-
+    options = params.map { |k, v| "-#{k} #{v.inspect}" }.join(" ")
     `#{BIN_PATH} #{options}`
+  end
+
+  private def self.check_execute
+    if !can_execute?
+      raise "#{BIN_PATH} missing: run  brew install terminal-notifier" 
+    end
+  end
+
+  private def self.can_execute? : Bool
+    `which #{BIN_PATH} >/dev/null 2>&1`
+    $?.success?
   end
 end
